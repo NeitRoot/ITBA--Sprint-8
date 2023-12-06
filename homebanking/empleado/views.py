f9f082ae98394df539571d4205b69fd2daf89e3d
Natalia Anahí Vizcarra Savino <95430663+NeitRoot@users.noreply.github.com>
2023-12-06 12:41:14 -0300

    Add files via upload

--- homebanking/empleado/views.py
+++ homebanking/empleado/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Empleado
+from .serializers import EmpleadoSerializer
+
+
+class EmpleadoViewSet(viewsets.ModelViewSet):
+    queryset = Empleado.objects.all()
+    serializer_class = EmpleadoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+         serializer.save(owner=self.request.user)
