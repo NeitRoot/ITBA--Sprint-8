ee17a0bf5c3887cbfc048d573d68f84f900df99b
Natalia Anahí Vizcarra Savino <95430663+NeitRoot@users.noreply.github.com>
2023-12-06 20:34:21 -0300

    Add files via upload

--- homebanking/cliente/models.py
+++ homebanking/cliente/models.py
@@ -1,15 +1,18 @@
 from django.db import models
-# Create your models here.
-# from sucursal.models import Sucursal
-# from tarjeta.models import Tarjetas
+
 
 from django.contrib.auth.models import User
+
 
 class Cliente(models.Model):
     customer_name = models.TextField()
     customer_surname = models.TextField()
     customer_dni = models.TextField(db_column='customer_DNI')
     dob = models.TextField(blank=True, null=True)
+    
+    customer_adress = models.ForeignKey('direccion.Direccion', on_delete=models.CASCADE, default=None, null=True )
+    type = models.CharField (max_length=10, default="CLASSIC", choices=(('BLACK', 'BLACK'), ('GOLD','GOLD'), ('CLASSIC', 'CLASSIC')))
+    
     branch = models.ForeignKey('sucursal.Sucursal', models.DO_NOTHING)
     card = models.ForeignKey('tarjeta.Tarjetas', models.DO_NOTHING)
 
@@ -20,4 +23,4 @@ class Cliente(models.Model):
         verbose_name = "Cliente"
 
     def __str__(self) -> str:
-        return self.customer_name
+        return self.customer_name + " " + self.customer_surname + " "+ str(self.customer_dni)
